08a2748fefd03e4bdff7c484fa54badea2fc7e46
Divikiran Ravela <divikiran.ravela@neudesic.com>
2017-06-24 13:16:18 +0530

    Android support for userdialog
    Android support for userdialog

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Acr.UserDialogs;
 
 namespace AglTestApp.Droid
 {
@@ -17,7 +18,7 @@ namespace AglTestApp.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            UserDialogs.Init(this);
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
